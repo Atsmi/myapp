67382957da6005ca5e8ca38b98df7e5df949f38c
LAPTOP-E0D99GDE\mya-m <sea.of.greentea.a@gmail.com>
2016-11-01 22:40:08 +0900

    kakeiテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161101104253) do
+
+  create_table "kakeis", force: :cascade do |t|
+    t.date     "day"
+    t.integer  "price",      limit: 4
+    t.string   "use",        limit: 255
+    t.text     "text",       limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
