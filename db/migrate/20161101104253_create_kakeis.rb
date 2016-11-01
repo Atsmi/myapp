893d14cf12e8e747529589273882f9b0ae9c8e4c
LAPTOP-E0D99GDE\mya-m <sea.of.greentea.a@gmail.com>
2016-11-01 21:34:06 +0900

    マイグレーションファイル変更（カラム編集）

--- db/migrate/20161101104253_create_kakeis.rb
+++ db/migrate/20161101104253_create_kakeis.rb
@@ -1,8 +1,11 @@
 class CreateKakeis < ActiveRecord::Migration
   def change
     create_table :kakeis do |t|
-
-      t.timestamps null: false
+      t.date     :day
+      t.integer  :amount_of_money
+      t.string   :use
+      t.text     :text
+      t.timestamps
     end
   end
 end
