93b7cac295a9925a04ecebfec1b35638af6a021c
LAPTOP-E0D99GDE\mya-m <sea.of.greentea.a@gmail.com>
2016-11-16 16:47:43 +0900

    deviseメソッド追加・createviewファイル追加・indexviewファイル削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
   Rails.application.routes.draw do
   devise_for :users
+    root 'kakei#index'
     get 'kakei' => 'kakei#index'
     get 'kakei/new' => 'kakei#new'
     post 'kakei' => 'kakei#create'
